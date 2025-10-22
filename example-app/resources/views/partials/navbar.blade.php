54267d22e46c23360b86c0d94c2f57a64abea944
AnandaCahyaRamadan <76020880+AnandaCahyaRamadan@users.noreply.github.com>
2025-10-22 11:14:45 +0700

    Fix navbar

--- example-app/resources/views/partials/navbar.blade.php
+++ example-app/resources/views/partials/navbar.blade.php
@@ -1,15 +1,48 @@
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-    </ul>
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+  <!-- Tombol Sidebar -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" data-widget="pushmenu" href="#" role="button">
+        <i class="fas fa-bars"></i>
+      </a>
+    </li>
+  </ul>
 
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li>
-    </ul>
-  </nav>
+  <!-- Menu Kanan -->
+  <ul class="navbar-nav ml-auto">
+    <!-- Fullscreen -->
+    <li class="nav-item">
+      <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+        <i class="fas fa-expand-arrows-alt"></i>
+      </a>
+    </li>
+
+    <!-- User Dropdown -->
+    <li class="nav-item dropdown user-menu">
+      <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+        <img src="dist/img/user2-160x160.jpg" class="user-image img-circle elevation-2" alt="User Image">
+        <span class="d-none d-md-inline">Administrator</span>
+      </a>
+      <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+        <li class="user-header bg-primary">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <p>Administrator</p>
+        </li>
+
+        <li class="user-footer">
+          <a href="#" class="btn btn-default btn-flat text-danger float-right" onclick="logout()">
+            <i class="fas fa-sign-out-alt"></i> Logout
+          </a>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</nav>
+
+<script>
+  function logout() {
+    if (confirm('Yakin ingin logout?')) {
+      window.location.href = '/logout';
+    }
+  }
+</script>
--- example-app/resources/views/partials/sidebar.blade.php
+++ example-app/resources/views/partials/sidebar.blade.php
@@ -8,16 +8,6 @@
 
   <!-- Sidebar -->
   <div class="sidebar">
-    <!-- Sidebar user panel -->
-    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-      <div class="image">
-        <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-      </div>
-      <div class="info">
-        <a href="#" class="d-block">Administrator</a>
-      </div>
-    </div>
-
     <!-- Sidebar Menu -->
     <nav class="mt-2">
       <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
@@ -36,7 +26,7 @@
           <a href="#" class="nav-link">
             <i class="nav-icon fas fa-graduation-cap"></i>
             <p>
-              Penyelenggara Pelatihan
+              Penyelenggara
               <i class="fas fa-angle-left right"></i>
             </p>
           </a>
