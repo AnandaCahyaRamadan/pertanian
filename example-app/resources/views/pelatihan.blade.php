4d303b28c1900de821803c0d7db57f947a6a71e4
AnandaCahyaRamadan <76020880+AnandaCahyaRamadan@users.noreply.github.com>
2025-10-23 21:56:46 +0700

    fix color

--- example-app/resources/views/pelatihan.blade.php
+++ example-app/resources/views/pelatihan.blade.php
@@ -14,9 +14,9 @@
                              class="rounded-3" style="width: 100px">
                     </div>
                     <div class="pelatih-info text-start flex-fill text-white">
-                        <p class="fw-bold mb-3">{{ $item->pegawai->name }}</p>
-                        <p class="mb-1">{{ $item->pegawai->position }}</p>
-                        <p class="mb-3">{{ $item->keahlian->keahlian_name }}</p>
+                        <p class="fw-bold mb-3 text-dark">{{ $item->pegawai->name }}</p>
+                        <p class="mb-1 text-dark">{{ $item->pegawai->position }}</p>
+                        <p class="mb-3 text-dark">{{ $item->keahlian->keahlian_name }}</p>
                         <a href="https://wa.me/{{ $item->pegawai->no_wa }}" 
                            target="_blank" 
                            class="btn btn-light btn-sm rounded-pill px-3 py-1">
